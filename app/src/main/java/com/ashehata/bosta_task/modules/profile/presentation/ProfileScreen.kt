84f39f00565bf8e27120dce4ed7d445f50daf5cd
ahmed shehata <ahamdyshehata7@gmail.com>
2023-06-08 15:49:45 +0300

    Edit ProfileScreen

--- app/src/main/java/com/ashehata/bosta_task/modules/profile/presentation/ProfileScreen.kt
+++ app/src/main/java/com/ashehata/bosta_task/modules/profile/presentation/ProfileScreen.kt
@@ -1,6 +1,12 @@
 package com.ashehata.bosta_task.modules.profile.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.ashehata.bosta_task.R
+import com.ashehata.bosta_task.common.presentation.AppBar
 import com.ramcosta.composedestinations.annotation.Destination
 
 
@@ -8,4 +14,27 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Destination(start = true)
 fun ProfileScreen() {
 
+    Column {
+
+        AppBar(
+            title = stringResource(id = R.string.profile),
+            extraBody = {
+                Text(
+                    text = "Ahmed Hamdy",
+                    style = MaterialTheme.typography.body1.copy(
+                        color = MaterialTheme.colors.onSecondary
+                    )
+                )
+                Text(
+                    text = "76 EL Madeena Al Monawara St., Al Dakahlia",
+                    style = MaterialTheme.typography.body1.copy(
+                        color = MaterialTheme.colors.onSecondary
+                    )
+                )
+            }
+        )
+
+
+
+    }
 }

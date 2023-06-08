00cdc35c0a6fcc69b1cb7edcc794544d250aaeae
ahmed shehata <ahamdyshehata7@gmail.com>
2023-06-08 15:34:39 +0300

    Add ProfileScreen file

--- app/src/main/java/com/ashehata/bosta_task/home/HomeActivity.kt
+++ app/src/main/java/com/ashehata/bosta_task/home/HomeActivity.kt
@@ -3,25 +3,15 @@ package com.ashehata.bosta_task.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
-import com.ashehata.bosta_task.ui.theme.Bosta_TaskTheme
+import com.ashehata.bosta_task.modules.profile.presentation.ProfileScreen
+import com.ashehata.bosta_task.ui.theme.AppTheme
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Bosta_TaskTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Text(text = "Hello")
-                }
+            AppTheme {
+                ProfileScreen()
             }
         }
     }
---
+++ app/src/main/java/com/ashehata/bosta_task/modules/profile/presentation/ProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.ashehata.bosta_task.modules.profile.presentation
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+
+@Composable
+@Destination(start = true)
+fun ProfileScreen() {
+
+}
